21d34fd52db3137a7b72c9eafcc7e79d66a0c4cd
etsheep <754410651@qq.com>
2016-08-21 13:08:16 +0800

    23

--- Test.java
+++ Test.java
@@ -2,6 +2,22 @@ public class Test{
 	
 	public void test(){
 		System.out.println("aaa");
+		System.out.println("aaa2");
+		System.out.println("aaa2");
+		System.out.println("aaa");
+		System.out.println("aaa");
+	}
+	
+	public void test1(){
+		System.out.println("aaa");
+	}
+	
+	public void test2(){
+		System.out.println("aaa");
+	}
+	
+	public void test3(){
+		System.out.println("aaa");
 	}
 	
 }
