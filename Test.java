3e923d1be4c55e86416dcdd074fe113c5d43134e
etsheep <754410651@qq.com>
2016-08-21 13:15:27 +0800

    121

--- Test.java
+++ Test.java
@@ -2,22 +2,24 @@ public class Test{
 	
 	public void test(){
 		System.out.println("aaa");
-		System.out.println("aaa2");
-		System.out.println("aaa2");
-		System.out.println("aaa");
+		System.out.println("aaa3");
+		System.out.println("aaa5");
+	}
+		public void test3x(){
 		System.out.println("aaa");
 	}
-	
 	public void test1(){
 		System.out.println("aaa");
 	}
 	
 	public void test2(){
+		A a = new A();
+		B b = new B();
+		a = b;
+		return;
+	}
+	}
+		public void test3(){
 		System.out.println("aaa");
 	}
-	
-	public void test3(){
-		System.out.println("aaa");
-	}
-	
 }
