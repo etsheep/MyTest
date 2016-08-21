e3632226c77f85cce4b210f3a3b09669b185f593
etsheep <754410651@qq.com>
2016-08-21 13:05:00 +0800

    123

--- Test.java
+++ Test.java
@@ -1,24 +1,7 @@
 public class Test{
-
-	public void test123123(){
-		System.out.println("23222");
-		System.out.println("adfxvxcv");
-		System.out.println("aasdfxc2323zxca");
-	}
-
+	
 	public void test(){
 		System.out.println("aaa");
 	}
 	
-	
-	public void testsdadf1232222(){
-		System.out.println("aasdfxc2323zxca");
-		System.out.println("aasd213fxczxca");
-	}
-	
-	public void testsdad123f(){
-		System.out.println("12322");
-		System.out.println("aasdfxc2323zxca");
-	}
-
 }
