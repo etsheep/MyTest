11a213bbcc525480368c5597b5f252aeb864e380
etsheep <754410651@qq.com>
2016-08-21 12:26:25 +0800

    123

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ public class Test{
 
 	public void test123123(){
 		System.out.println("adfxvxcv");
+		System.out.println("aasdfxc2323zxca");
 	}
 
 	public void test(){
@@ -13,13 +14,5 @@ public class Test{
 		System.out.println("aasdfxc2323zxca");
 		System.out.println("aasdfxczxca");
 	}
-	
-	public void testsdad123f(){
-		System.out.println("aasdfxc2323zxca");
-	}
 
-
-	public void test123(){
-		System.out.println("adfxvxcv");
-	}
 }
